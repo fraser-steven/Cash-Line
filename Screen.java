5c9b038b5f697784f9ca5c0a5da168bfdf631980
fraser-steven <fraser.squash@gmail.com>
2024-01-31 13:43:56 +0000

    added starting code

--- Screen.java
+++ Screen.java
@@ -1,3 +1,17 @@
+import java.io.IOException;
+
 public class Screen {
     
+    public static void nain(String[] args) throws IOException {
+
+        Options options = new Options();
+        introduction();
+        options.mainMenu();
+
+    }
+
+    public static void introduction() {
+
+        System.out.println("Welcome to the Cash Line");
+    }
 }
